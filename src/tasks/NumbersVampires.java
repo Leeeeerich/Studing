1731a67a4f00bb71af6a7ce0c98233452013adbf
Valerii Sosov <sosov.valeriy@gmail.com>
2018-12-10 15:45:36 +0200

    Update Logic

--- src/tasks/NumbersVampires.java
+++ src/tasks/NumbersVampires.java
@@ -3,15 +3,25 @@ package tasks;
 public class NumbersVampires {
 
     public NumbersVampires(int firstNum, int secondNum, int numberOf) {
-        for (int i = firstNum; i < numberOf + 1; i++) {
+        for (int i = firstNum; i < numberOf; i++) {
             if (isTrailingZero(i)) continue;
+
             for (int j = secondNum; j < numberOf; j++) {
                 if (isTrailingZero(j)) continue;
+
                 int resultMultiple = i * j;
                 if (isEvenNumbersResult(resultMultiple)) continue;
 
-                if (String.valueOf(resultMultiple).contains(String.valueOf(i)) &
-                        String.valueOf(resultMultiple).contains(String.valueOf(j))) {
+                char c = String.valueOf(i).charAt(0);
+                char c1 = String.valueOf(i).charAt(1);
+
+                char c2 = String.valueOf(j).charAt(0);
+                char c3 = String.valueOf(j).charAt(1);
+
+                if (String.valueOf(resultMultiple).contains(String.valueOf(c)) &
+                        String.valueOf(resultMultiple).contains(String.valueOf(c1)) &
+                        String.valueOf(resultMultiple).contains(String.valueOf(c2)) &
+                        String.valueOf(resultMultiple).contains(String.valueOf(c3))) {
                     System.out.println(i + " x " + j + " = " + resultMultiple);
                 }
             }
